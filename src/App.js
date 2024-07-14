59d162aaa8983ac9430c27ab55781ea4ed798006
ajaydevra <ajaydevra1999@gmai.com>
2024-07-14 12:38:29 +0530

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from './components/Home'
+import Contact from './components/Contact'
+import About from './components/About'
+import Login from './components/Login'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
 
-export default App;
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={Login} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/about" component={About} />
+    <ProtectedRoute exact path="/contact" component={Contact} />
+    <Route component={NotFound} />
+  </Switch>
+)
+
+export default App
---
+++ src/components/Contact/index.js
@@ -0,0 +1,15 @@
+import LogoutButton from '../LogoutButton'
+import Header from '../Header'
+import './index.css'
+
+const Contact = () => (
+  <>
+    <Header />
+    <div className="about">
+      <h1>Contact Route</h1>
+      <LogoutButton />
+    </div>
+  </>
+)
+
+export default Contact
